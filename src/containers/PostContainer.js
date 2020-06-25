127bd5e4d24a7d962323dcf26619a6cbc4a052ee
codeminnam <code.minnam@gmail.com>
2020-06-25 17:42:17 +0900

    사용자 경험 개선 (포스트 데이터 상태 구조 바꾸기)

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Post from '../components/Post';
-import { getPost, clearPost } from '../modules/posts';
+import { getPost } from '../modules/posts';
+import { reducerUtils } from '../lib/asyncUtils';
 
 function PostContainer({ postId }) {
-    const { data, loading, error } = useSelector(state => state.posts.post);
+    const { data, loading, error } = useSelector(
+        state => state.posts.post[postId] || reducerUtils.initial()
+    );
     const dispatch = useDispatch();
 
     useEffect(() => {
+        if (data) return;
         dispatch(getPost(postId));
-        return () => {
-            dispatch(clearPost());
-        }
     }, [dispatch, postId]);
 
-    if (loading) return <div>loading..</div>;
+    if (loading && !data) return <div>loading..</div>;
     if (error) return <div>error!</div>;
     if (!data) return null;
 
