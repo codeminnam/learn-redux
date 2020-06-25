7fe57668350210d4e1a8b88dd5200959ac807194
codeminnam <code.minnam@gmail.com>
2020-06-25 13:16:24 +0900

    redux-thunk로 Promise 다루기 (Router)

---
+++ src/containers/PostContainer.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Post from '../components/Post';
+import { getPost } from '../modules/posts';
+
+function PostContainer({ postId }) {
+    const { data, loading, error } = useSelector(state => state.posts.post);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPost(postId));
+    }, [dispatch, postId]);
+
+    if (loading) return <div>loading..</div>;
+    if (error) return <div>error!</div>;
+    if (!data) return null;
+
+    return (
+        <Post post={data} />
+    );
+}
+
+export default PostContainer;
