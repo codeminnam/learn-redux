a10524c4dd3ea23702b75cda0c9f8c14faaa1653
codeminnam <code.minnam@gmail.com>
2020-06-25 11:23:34 +0900

    Redux-thunk로 Promise 다루기 (Refactoring)

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postAPI from '../api/posts';
-import { reducerUtils } from '../lib/asyncUtils';
+import { reducerUtils, createPromiseThunk, handleAsyncActions } from '../lib/asyncUtils';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -9,75 +9,27 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-export const getPosts = () => async dispatch => {
-    dispatch({ type: GET_POSTS });
-    try {
-        const posts = await postsAPI.getPosts();
-        dispatch({
-            type: GET_POSTS_SUCCESS,
-            posts
-        });
-    } catch (e) {
-        dispatch({
-            type: GET_POSTS_ERROR,
-            error: e
-        })
-    }
-}
-
-export const getPost = (id) => async dispatch => {
-    dispatch({ type: GET_POST });
-    try {
-        const post = await postsAPI.getPostById(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            post
-        });
-    } catch (e) {
-        dispatch({
-            type: GET_POST_ERROR,
-            error: e
-        })
-    }
-}
+export const getPosts = createPromiseThunk(GET_POSTS, postAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postAPI.getPostById);
 
 const initialState = {
     posts: reducerUtils.initial(),
     post: reducerUtils.initial()
 }
 
+const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts');
+const getPostReducer = handleAsyncActions(GET_POST, 'post');
+
 export default function posts(state = initialState, action) {
     switch (action.type) {
         case GET_POSTS:
-            return {
-                ...state,
-                posts: reducerUtils.loading()
-            };
         case GET_POSTS_SUCCESS:
-            return {
-                ...state,
-                posts: reducerUtils.success(action.posts)
-            };
         case GET_POSTS_ERROR:
-            return {
-                ...state,
-                posts: reducerUtils.error(action.error)
-            };
+            return getPostsReducer(state, action);
         case GET_POST:
-            return {
-                ...state,
-                post: reducerUtils.loading()
-            };
         case GET_POST_SUCCESS:
-            return {
-                ...state,
-                post: reducerUtils.success(action.post)
-            };
         case GET_POST_ERROR:
-            return {
-                ...state,
-                post: reducerUtils.error(action.error)
-            };
+            return getPostReducer(state, action);
         default:
             return state;
     }
