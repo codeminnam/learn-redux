28bb17409aef56b3914ce5be26594a495b8aac8d
codeminnam <code.minnam@gmail.com>
2020-06-27 00:18:30 +0900

    사용자 경험 개선 (createPromiseThunkById, handleAsyncActionsById)

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,24 +1,3 @@
-export const createPromiseThunk = (type, promiseCreator) => {
-    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
-
-    return param => async dispatch => {
-        dispatch({ type, param });
-        try {
-            const payload = await promiseCreator(param);
-            dispatch({
-                type: SUCCESS,
-                payload
-            });
-        } catch (e) {
-            dispatch({
-                type: ERROR,
-                payload: e,
-                error: true
-            });
-        }
-    };
-};
-
 export const reducerUtils = {
     initial: (initialData = null) => ({
         loading: false,
@@ -40,6 +19,52 @@ export const reducerUtils = {
         data: null,
         error: error
     })
+}
+
+export const createPromiseThunk = (type, promiseCreator) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+    return param => async dispatch => {
+        dispatch({ type, param });
+        try {
+            const payload = await promiseCreator(param);
+            dispatch({
+                type: SUCCESS,
+                payload
+            });
+        } catch (e) {
+            dispatch({
+                type: ERROR,
+                payload: e,
+                error: true
+            });
+        }
+    };
+};
+
+const defaultIdSelector = param => param;
+export const createPromiseThunkById = (type, promiseCreator, idSelector = defaultIdSelector) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+    return param => async dispatch => {
+        const id = idSelector(param);
+        dispatch({ type, meta: id });
+        try {
+            const payload = await promiseCreator(param);
+            dispatch({
+                type: SUCCESS,
+                payload,
+                meta: id
+            });
+        } catch (e) {
+            dispatch({
+                type: ERROR,
+                payload: e,
+                error: true,
+                meta: id
+            });
+        }
+    };
 }
 
 export const handleAsyncActions = (type, key, keepData) => {
@@ -67,3 +92,37 @@ export const handleAsyncActions = (type, key, keepData) => {
     };
 };
 
+export const handleAsyncActionsById = (type, key, keepData) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    return (state, action) => {
+        const id = action.meta;
+        switch (action.type) {
+            case type:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.loading(keepData ? (state[key][id] && state[key][id].data) : null)
+                    }
+                }
+            case SUCCESS:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.success(action.payload)
+                    }
+                }
+            case ERROR:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.error(action.payload)
+                    }
+                }
+            default:
+                return state;
+        }
+    };
+};
