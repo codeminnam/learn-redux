3263df82a51cf820690926099417b25e7d7e400d
codeminnam <code.minnam@gmail.com>
2020-06-29 16:45:31 +0900

    json-server & proxy

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,29 +1,11 @@
-const sleep = n => new Promise(resolve => setTimeout(resolve, n));
-
-const posts = [
-    {
-        id: 1,
-        title: '멍!',
-        body: '우디오디우디우디오디'
-    },
-    {
-        id: 2,
-        title: 'wof!',
-        body: 'me missin elsa even though i never met her'
-    },
-    {
-        id: 3,
-        title: 'わん!',
-        body: '日本にいる間知り合いの犬ってなかったんだ、、一匹も'
-    }
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-    await sleep(500);
-    return posts;
+    const response = await axios.get('/posts');
+    return response.data;
 };
 
 export const getPostById = async id => {
-    await sleep(500);
-    return posts.find(post => post.id === id);
+    const response = await axios.get(`/posts/${id}`);
+    return response.data;
 };
